7cd5d49663a97751e09c20c5399274f8e095736e
energister <energister@users.noreply.github.com>
2019-05-05 21:20:42 +0300

    adopt latest Spring

--- src/test/java/hello/mvc/ServiceBaseUrlInjectionTest.java
+++ src/test/java/hello/mvc/ServiceBaseUrlInjectionTest.java
@@ -5,18 +5,15 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import javax.annotation.Resource;
 import java.net.URL;
 
 import static org.hamcrest.Matchers.equalTo;
-import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
 
 @RunWith(SpringRunner.class)
@@ -37,7 +34,7 @@ public class ServiceBaseUrlInjectionTest {
     }
 
     @Test
-    public void getHello() throws Exception {
+    public void getHello() {
         ResponseEntity<String> response = template.getForEntity(serviceBaseUrl.toString(),
                                                                 String.class);
         assertThat(response.getBody(), equalTo(HelloController.GREETING));
